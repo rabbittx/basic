d5f9eeb54d39b272b8e4e385b26e4d0bf167dd1d
rabbitt <rabbitt1991x@gmail.com>
2023-01-22 01:04:31 +0330

    there is no docs for engine room!.

--- python_basic/main.py
+++ python_basic/main.py
@@ -49,21 +49,67 @@ def find_bmm():
 
 
 #Simple Graph Plotting in Python -------------------------------------------------------------------------------------
-from random import randint
-dice = [0,0,0,0,0,0]
-for i in range(10000):
-    dice_roll  = randint(1,6)
-    if dice_roll == 1 :
-        dice[0] +=1
-    elif dice_roll == 2 :
-        dice[1] +=1
-    elif dice_roll == 3 :
-        dice[2] +=1
-    elif dice_roll == 4 :
-        dice[3] +=1
-    elif dice_roll == 5 :
-        dice[4] +=1
-    elif dice_roll == 6 :
-        dice[5] +=1
 
-print(dice)
+import random 
+
+def roll_dice():
+    # random.seed(23*99-6%60) get random 4 -.-
+    dice = random.randint(1,6)
+    return dice
+
+dice_list = [0,0,0,0,0,0]
+for i in range(10000):
+    dice  = roll_dice()
+    # print(dice)
+    if dice == 1 :
+        dice_list[0] +=1
+    elif dice == 2 :
+        dice_list[1] +=1
+    elif dice == 3 :
+        dice_list[2] +=1
+    elif dice == 4 :
+        dice_list[3] +=1
+    elif dice == 5 :
+        dice_list[4] +=1
+    elif dice == 6 :
+        dice_list[5] +=1
+
+import numpy as np
+import pandas as pd
+from matplotlib.pyplot import plot
+# output_notebook() # uncomment this for use in a notebook
+
+SIZES = (10, 50, 100, 500, 1000, 5000, 10000, 50000)
+BINS = np.arange(14) - 0.5
+random.seed(5864218/8*236)
+def roll():
+    return random.randint(1,6)
+
+roll_dice_number = 0
+import numpy as np
+import matplotlib.pyplot as plt
+dice_dic = {1:0,2:0,3:0,4:0,5:0,6:0}
+# creating the dataset
+for i in range(10000):
+    data = roll()
+    if data == 1 :
+        dice_dic[1] +=1
+    elif data == 2 :
+        dice_dic[2] +=1
+    elif data == 3 :
+        dice_dic[3] +=1
+    elif data == 4 :
+        dice_dic[4] +=1
+    elif data == 5 :
+        dice_dic[5] +=1
+    elif data == 6 :
+        dice_dic[6] +=1
+
+
+print(dice_dic)
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+plt.show()
